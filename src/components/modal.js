9f120d822d7419199cbc9dd890e2982da6c41b29
Egor Ermilov <egorka33rus52@gmail.com>
2024-04-20 13:53:28 +0300

    Create modal component

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,3 +1,18 @@
-export const openModal = () => {};
+export const modalOpenedModifier = 'popup_is-opened';
 
-export const closeModal = () => {};
+export const openModal = ({ closeModalEsc, modalElement }) => {
+    modalElement.classList.add(modalOpenedModifier);
+    document.addEventListener('keydown', closeModalEsc);
+};
+
+export const closeModal = ({ closeModalEsc, modalElement }) => {
+    modalElement.classList.remove(modalOpenedModifier);
+    document.removeEventListener('keydown', closeModalEsc);
+};
+
+export const closeModalEsc = ({ key }) => {
+    if (key === 'Escape') {
+        const openedModal = document.querySelector('.popup_is-opened');
+        closeModal({ modalElement: openedModal });
+    }
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,41 @@
 import { createCard, deleteCard } from '../components/card.js';
+import { closeModal, closeModalEsc, openModal } from '../components/modal.js';
 import '../pages/index.css';
 import initialCards from './cards.js';
 
 const listElement = document.querySelector('.places__list');
 
 initialCards.forEach((cardData) => listElement.append(createCard({ cardData, deleteCard })));
+
+const modals = [
+    {
+        modalElement: document.querySelector('.popup_type_new-card'),
+        triggerElement: document.querySelector('.profile__add-button'),
+    },
+    {
+        modalElement: document.querySelector('.popup_type_edit'),
+        triggerElement: document.querySelector('.profile__edit-button'),
+    },
+    {
+        modalElement: document.querySelector('.popup_type_image'),
+        triggerElement: document.querySelectorAll('.card__image'),
+    },
+];
+
+modals.forEach((modal) => {
+    if (!(modal.triggerElement instanceof NodeList)) {
+        modal.triggerElement = [modal.triggerElement];
+    }
+
+    modal.triggerElement.forEach((buttonElement) => {
+        buttonElement.addEventListener('click', () => {
+            openModal({ closeModalEsc, modalElement: modal.modalElement });
+        });
+    });
+
+    modal.modalElement.addEventListener('click', ({ target }) => {
+        if (target.classList.contains('popup__close') || target.classList.contains('popup')) {
+            closeModal({ closeModalEsc, modalElement: modal.modalElement });
+        }
+    });
+});
