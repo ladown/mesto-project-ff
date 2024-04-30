d0d0a5d4e28525c5f24ab36a1c3a133ff0ff76d3
Egor Ermilov <egorka33rus52@gmail.com>
2024-04-30 23:45:58 +0300

    Add shared for validationConfig

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,11 +1,4 @@
-export const validationConfig = {
-    errorClass: 'popup__error_visible',
-    formSelector: '.popup__form',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-};
+import { validationConfig } from '../scripts/shared.js';
 
 export const isValidImageUrl = async ({ formElement, inputElement }) => {
     return fetch(inputElement.value, {
---
+++ src/scripts/shared.js
@@ -0,0 +1,8 @@
+export const validationConfig = {
+    errorClass: 'popup__error_visible',
+    formSelector: '.popup__form',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+};
